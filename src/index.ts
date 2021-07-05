e92d60a4cc5aad6cca486ba4d73bc651a5b71382
Damien Da Silva Bregieiro <damien.bregieiro@gmail.com>
2021-07-05 17:00:52 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,48 +11,53 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const startServer = async () => {
-  console.log('=================');
-
-  await createConnection({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: 3306,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'ELI',
-    entities: [__dirname + '/entity/*.ts'],
-    synchronize: true,
-  });
-
-  console.log('Synchronized');
-
-  const schema = await buildSchema({
-    resolvers: [__dirname + '/resolvers/*.{ts,js}'],
-    authChecker: passwordAuthChecker,
-    nullableByDefault: true,
-  });
-
-  console.log('Schematized');
-
-  const app = Express();
-  app.use(cors());
-  app.use(cookieParser());
-  const server = new ApolloServer({
-    schema,
-    introspection: true,
-    playground: true,
-    context: ({ req, res }) => ({ req, res }),
-  });
-
-  console.log('Expressed');
-
-  server.applyMiddleware({ app });
-
-  app.listen(4300, () => {
-    console.log('Server started');
-
+  try {
     console.log('=================');
-  });
+
+    await createConnection({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'ELI',
+      entities: [__dirname + '/entity/*.ts'],
+      synchronize: true,
+      logging: true,
+    });
+
+    console.log('Synchronized');
+
+    const schema = await buildSchema({
+      resolvers: [__dirname + '/resolvers/*.{ts,js}'],
+      authChecker: passwordAuthChecker,
+      nullableByDefault: true,
+    });
+
+    console.log('Schematized');
+
+    const app = Express();
+    app.use(cors());
+    app.use(cookieParser());
+    const server = new ApolloServer({
+      schema,
+      introspection: true,
+      playground: true,
+      context: ({ req, res }) => ({ req, res }),
+    });
+
+    console.log('Expressed');
+
+    server.applyMiddleware({ app });
+
+    app.listen(4300, () => {
+      console.log('Server started');
+
+      console.log('=================');
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 startServer().catch((e) => {
