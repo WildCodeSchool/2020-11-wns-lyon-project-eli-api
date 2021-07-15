f5da327b76d2b3efc11cfb42786fb0e28a75f343
Damien Da Silva Bregieiro <damien.bregieiro@gmail.com>
2021-07-15 15:09:45 +0200

    fix(

--- src/entity/QuizResults.ts
+++ src/entity/QuizResults.ts
@@ -42,7 +42,7 @@ export class QuizResults extends BaseEntity {
   @JoinTable({ name: 'quizresults_have_responses' })
   responses!: Response[];
 
-  @Field()
+  @Field(() => User)
   @ManyToOne(() => User, (user) => user.quizResults)
   user!: User;
 }
